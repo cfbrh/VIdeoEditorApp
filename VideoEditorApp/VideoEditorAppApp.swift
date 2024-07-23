414d578530dd0d6a0332229ba1a08c87e82961e2
张 <wz2647@columbia.edu>
2024-07-22 23:23:44 -0400

    Initial Commit

---
+++ VideoEditorApp/VideoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorAppApp.swift
+//  VideoEditorApp
+//
+//  Created by 张 on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
